5b17dda50a676b065ec847ae0316b4b9837b7d46
Muhammad waseem <waseem.m@dplit.com>
2022-06-06 15:51:29 +0500

    sfd

--- spring-boot-hello-world-example/src/main/java/com/helloworld/controller/HelloWorldController.java
+++ spring-boot-hello-world-example/src/main/java/com/helloworld/controller/HelloWorldController.java
@@ -7,6 +7,6 @@ public class HelloWorldController
 @RequestMapping("/")
 public String hello() 
 {
-return "<h1> Congratulations CDK project. You have successfully deployed the sample Spring Boot Application . </h1>";
+return "<h1> Congratulations waseem. You have successfully deployed the sample Spring Boot Application . </h1>";
 }
 }
